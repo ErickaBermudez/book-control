ec52e7b2c17c032c8759cf88caebf982e05efe50
ericka <erickabdz@gmail.com>
2019-02-25 14:27:30 -0700

    Saves xml file

--- app/controllers/Books.java
+++ app/controllers/Books.java
@@ -1,23 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package controllers;
-
-import java.util.*;
-import java.util.logging.Level;
-
-import models.*;
-import play.*;
-import play.db.jpa.Blob;
-import play.mvc.*;
-
 /**
- *
  * @author Ericka Bermudez
  * @date 21/02/2019
  */
+package controllers;
+
+import java.io.*;
+import java.util.*;
+import java.util.logging.Level;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import models.*;
+
+/* Imports for JDOM */
+import org.jdom2.*;
+import org.jdom2.Content.CType;
+import org.jdom2.filter.Filters;
+import org.jdom2.input.*;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+import org.jdom2.util.IteratorIterable;
+import play.mvc.*;
+
 public class Books extends Controller {
 
     public static void newBook() {
@@ -31,12 +35,12 @@ public class Books extends Controller {
     }
 
     public static void saveChanges(String id, String isbn, String name, String author) {
-        System.out.println("id = " + id);
+        // System.out.println("id = " + id);
         long idLong = Long.valueOf(id);
-        System.out.println("id: " + idLong);
+        // System.out.println("id: " + idLong);
         Book book = new Book();
         book = Book.findById(idLong);
-        System.out.println("Name: " + book.name);
+        //System.out.println("Name: " + book.name);
         book.setISBN(isbn);
         book.setName(name);
         book.setAuthor(author);
@@ -76,7 +80,7 @@ public class Books extends Controller {
             book.setAuthor(author);
             book.save();
             try {
-                Thread.sleep(3000);
+                Thread.sleep(2000);
             } catch (InterruptedException ex) {
                 java.util.logging.Logger.getLogger(Books.class.getName()).log(Level.SEVERE, null, ex);
             }
@@ -90,7 +94,68 @@ public class Books extends Controller {
         render(books);
     }
 
-    public static void delete(int id) {
+    public static void delete(Long id) {
+        Book book;
+        book = Book.findById(id);
+        book.delete();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            java.util.logging.Logger.getLogger(Books.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        bookList();
+    }
+
+    public static void generateXML(String[] args) {
+        List<Book> books = Book.findAll();
+        try {
+            Element list = new Element("list");
+            Document doc = new Document(list);
+
+            for (int i = 0; i < books.size(); i++) {
+                Book currentBook = books.get(i);
+                Element book = new Element("book");
+                book.setAttribute(new Attribute("id", Long.toString(currentBook.id)));
+                book.addContent(new Element("isbn").setText(currentBook.ISBN));
+                book.addContent(new Element("name").setText(currentBook.name));
+                book.addContent(new Element("author").setText(currentBook.author));
+                doc.getRootElement().addContent(book);
+            }
+
+            // new xmloutputter
+            XMLOutputter xmlOutput = new XMLOutputter();
+
+            // display nice nice
+            xmlOutput.setFormat(Format.getPrettyFormat());
+            xmlOutput.output(doc, new FileWriter("c:\\books.xml"));
+
+            System.out.println("File saved!");
+        }catch(IOException io){
+            System.out.println(io.getMessage());
+        }
 
     }
+
+    public static void readXML(String[] args) {
+        String fileName = "books.xml";
+        Document document = getSAXParsedDocument(fileName);
+
+        Element rootNode = document.getRootElement();
+        System.out.println("Root Element :: " + rootNode.getName());
+
+    }
+
+    private static Document getSAXParsedDocument(final String fileName) {
+        SAXBuilder builder = new SAXBuilder();
+        Document document = null;
+        try {
+            document = builder.build(fileName);
+        } catch (JDOMException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return document;
+    }
+
 }
